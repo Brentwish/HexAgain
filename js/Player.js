ed9aee67a8e65cb77f2f32ae5b46ee503792499f
brent <brent.wish@gmail.com>
2016-11-14 11:26:13 -0800

    fixed player bounds checking

--- js/Player.js
+++ js/Player.js
@@ -28,7 +28,7 @@ Player.prototype.rotate_left = function() {
 Player.prototype.move_forward = function() {
   h = hex_neighbor(this.hex, this.orientation);
   h = this.board.hex_at(h.q, h.r, h.s);
-  if (h.q) {
+  if (h) {
     this.board.update_hex(null, this.hex);
     this.hex = h;
     this.board.update_hex(this, this.hex);
@@ -38,7 +38,7 @@ Player.prototype.move_forward = function() {
 Player.prototype.move_backward = function() {
   h = hex_neighbor(this.hex, (this.orientation + 3) % 6);
   h = this.board.hex_at(h.q, h.r, h.s);
-  if (h.q) {
+  if (h) {
     this.board.update_hex(null, this.hex);
     this.hex = h;
     this.board.update_hex(this, this.hex);
